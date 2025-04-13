50abbf3eaf46eba001f46e9a79b127931f4859de
Ayushman Singh <ayushmans012@gmail.com>
2025-04-13 17:40:05 +0530

    enemy moves randomly

--- game/src/enemy/Demon.ts
+++ game/src/enemy/Demon.ts
@@ -5,14 +5,38 @@ enum Direction {
     RIGHT
 }
 
+const randomDirection = (exclude:  Direction) => {
+    let newDirection = Phaser.Math.Between(0,3);
+    while(newDirection === exclude){
+        newDirection = Phaser.Math.Between(0,3);
+    };
+
+    return newDirection
+}
+
 export default class Demon extends Phaser.Physics.Arcade.Sprite{
     private direction = Direction.DOWN;
+    private movement : Phaser.Time.TimerEvent;
+
     constructor(scene: Phaser.Scene, x:number, y:number, texture: string, frame?: string|number){
         super(scene,x,y,texture,frame );
 
         this.anims.play('demon-idle');
 
         scene.physics.world.on(Phaser.Physics.Arcade.Events.TILE_COLLIDE , this.handleTileCollision , this)
+
+        this.movement = scene.time.addEvent({
+            delay: 4000,
+            callback: () => {
+                this.direction = randomDirection(this.direction);
+            },
+            loop: true
+        })
+    }
+
+    destroy(fromScene?: boolean): void {
+        this.movement.destroy();
+        super.destroy(fromScene)
     }
 
     private handleTileCollision(go: Phaser.GameObjects.GameObject , tile: Phaser.Tilemaps.Tile){
@@ -20,8 +44,7 @@ export default class Demon extends Phaser.Physics.Arcade.Sprite{
             return
         }
 
-        const newDirection = Phaser.Math.Between(0,3);
-        this.direction = newDirection;
+        this.direction = randomDirection(this.direction);
 
     }
 
